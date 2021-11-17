e738fe0b11b599d29f91594d63d01df0f8ff02d5
Esteban Ramirez <netssrmrz@yahoo.com.au>
2021-11-17 19:24:37 +1100

    updates

--- public/libs/index.js
+++ public/libs/index.js
@@ -1,14 +1,17 @@
 import '/__/firebase/9.4.0/firebase-app-compat.js'
 import '/__/firebase/9.4.0/firebase-functions-compat.js'
+import '/__/firebase/9.4.0/firebase-analytics-compat.js'
 import '/__/firebase/init.js?useEmulator=true'
 
 class Context
 {
   constructor()
   {
-    const fb_fns = firebase.functions();
-    this.Register = fb_fns.httpsCallable('Register');
-    this.Save_Registration = fb_fns.httpsCallable('Save_Registration');
+    this.fb_anl = firebase.analytics();
+    this.fb_fns = firebase.functions();
+    
+    //this.Register = fb_fns.httpsCallable('Register');
+    //this.Save_Registration = fb_fns.httpsCallable('Save_Registration');
   }
 
   static Set_Id_Shortcuts(src_elem, dest_elem)
@@ -19,6 +22,26 @@ class Context
       const id = elem.id;
       dest_elem[id] = elem;
     }
+  }
+
+  async Register(to)
+  {
+    this.fb_anl.logEvent("sign_up", {method: "landing page"});
+
+    const fn = this.fb_fns.httpsCallable('Register');
+    const res = await fn(to);
+
+    return res;
+  }
+
+  async Save_Registration(form)
+  {
+    this.fb_anl.logEvent("save_form");
+
+    const fn = this.fb_fns.httpsCallable('Save_Registration');
+    const res = await fn(form);
+
+    return res;
   }
 }
 
